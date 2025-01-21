1d5d3f628590f799c81332eeb5b6ca9d1ecd51b9
ikalyancode <kalyan.psu@gmail.com>
2025-01-20 22:00:39 -0600

    projects merge

---
+++ employee-management/src/main/java/com/example/employee/model/Employee.java
@@ -0,0 +1,22 @@
+package com.example.employee.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String department;
+    private Double salary;
+}
